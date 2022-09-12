b003a6200fd2e138ad33aafdc5905dddd4be7140
john-seredich <seredichjohnalt@gmail.com>
2022-09-12 15:31:48 -0400

    feat: copy to clipboard and hover style

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,15 +1,37 @@
 import styles from "./Footer.module.scss";
 import { FaCopy, FaGithub, FaLinkedinIn } from "react-icons/fa";
+import { useEffect, useState } from "react";
 
 function Footer() {
+  const [alert, setAlert] = useState(false);
+  const email = "jsseredich@gmail.com";
+
+  const copyToClipboardHandler = () => {
+    navigator.clipboard.writeText(email);
+    setAlert(true);
+  };
+
+  useEffect(() => {
+    if (alert === true) {
+      const timer = setTimeout(() => {
+        setAlert(false);
+      }, 2000);
+
+      return () => {
+        clearTimeout(timer);
+      };
+    }
+  }, [alert]);
+
   return (
     <footer className={styles.footer}>
       <h2>Contact</h2>
       <p>I am open to opprtunities! Please feel free to reach out to me.</p>
-      <button className={styles.footer__copy}>
-        <span>jsseredich@gmail.com</span>
+      <button className={styles.footer__copy} onClick={copyToClipboardHandler}>
+        <span>{email}</span>
         <FaCopy />
       </button>
+      {alert && <p className={styles.footer__alert}>Copied email!</p>}
       <div className={styles.footer__links}>
         <a href="/">
           <FaGithub />
