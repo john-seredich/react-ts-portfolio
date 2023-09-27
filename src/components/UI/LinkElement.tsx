75d594ccd7cb1d4b9faa2a92e7a81cd13f057222
john-seredich <seredichjohnalt@gmail.com>
2023-09-27 12:34:45 -0400

    feat: add accent to resume in hero and link element

--- src/components/UI/LinkElement.tsx
+++ src/components/UI/LinkElement.tsx
@@ -1,4 +1,10 @@
-const links = ["About", "Projects", "Contact", "Resume"];
+const links = ["About", "Projects", "Contact", "Resumé"];
 export const linkElements = links.map((link: string) => {
-  return <li key={link}>{link}</li>;
+  return (
+    <li key={link}>
+      <a style={{ textDecoration: "none", color: "#dadada" }} href={`#${link}`}>
+        {link}
+      </a>
+    </li>
+  );
 });
