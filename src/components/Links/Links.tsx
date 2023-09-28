0a637735ef4f15dfc7b8def7d49f193fb8cf975c
john-seredich <seredichjohnalt@gmail.com>
2023-09-28 14:28:47 -0400

    feat: add resume link to link component

--- src/components/Links/Links.tsx
+++ src/components/Links/Links.tsx
@@ -1,14 +1,28 @@
 import React from "react";
 import styles from "./Links.module.scss";
 const links = ["About", "Projects", "Contact", "Resumé"];
+const resumeLink =
+  "https://docs.google.com/document/d/e/2PACX-1vT9pcz7A0CZed_idLFMJYZg61o5lu04YIPSPZZTzbkcA5BYvhn_fJZGHCaI-20YYQ/pub";
 
 function Links() {
   const link = links.map((link: string) => {
     return (
       <li className={styles.list_item}>
-        <a className={styles.link} key={link} href={`#${link}`}>
-          {link}
-        </a>
+        {link !== "Resumé" ? (
+          <a className={styles.link} key={link} href={`#${link}`}>
+            {link}
+          </a>
+        ) : (
+          <a
+            className={styles.link}
+            key={link}
+            href={resumeLink}
+            target="_blank"
+            rel="noreferrer"
+          >
+            Resumé
+          </a>
+        )}
       </li>
     );
   });
