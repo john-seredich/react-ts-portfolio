37a048bb9bb1df96e8af04314add5afe28cdc87e
john-seredich <seredichjohnalt@gmail.com>
2023-10-12 22:20:23 -0400

    rename: Frontend developer to Web Developer

--- src/components/Info/Info.tsx
+++ src/components/Info/Info.tsx
@@ -7,10 +7,10 @@ function Info() {
       <section className={styles.info__container}>
         <h2>About</h2>
         <p className={styles.info__description}>
-          My name is John Seredich a Front End Developer. Like many other
-          people, I enjoy increasing my knowledge of Web Development and
-          building new and challenging projects using React,Redux, Node.js and
-          many Front end technologies.
+          My name is John Seredich a Web Developer. Like many other people, I
+          enjoy increasing my knowledge of Web Development and building new and
+          challenging projects using React,Redux, Node.js and many Front end
+          technologies.
         </p>
       </section>
       <Skills />
