f0c3def317f7335d66373bbe20a15cc08e191455
john-seredich <seredichjohnalt@gmail.com>
2023-10-20 17:19:28 -0400

    feat: add id to each skill item

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -13,44 +13,47 @@ const skillItems = [
     name: "React",
     icon: <SiReact />,
     color: "#5dd2f3",
+    id: 1,
   },
   {
     name: "NextJS",
     icon: <SiNextdotjs />,
     color: "black",
+    id: 2,
   },
   {
     name: "Typescript",
     icon: <SiTypescript />,
     color: "#007acc",
+    id: 3,
   },
   {
     name: "Sass",
     icon: <SiSass />,
     color: "#c76395",
+    id: 4,
   },
   {
     name: "Git",
     icon: <SiGit />,
     color: "#f1502f",
+    id: 5,
   },
   {
     name: "Redux",
     icon: <SiRedux />,
     color: "#7248b6",
+    id: 6,
   },
 ];
 
-const skillItem = skillItems.map((item) => {
+const skillItem = skillItems.map(({ name, icon, color, id }) => {
   return (
-    <div className={styles.skills__grid__item}>
-      <div
-        style={{ color: item.color }}
-        className={styles.skills__grid__item__container}
-      >
-        {item.icon}
+    <div className={styles.skills__grid__item} key={id}>
+      <div style={{ color }} className={styles.skills__grid__item__container}>
+        {icon}
       </div>
-      <p>{item.name}</p>
+      <p>{name}</p>
     </div>
   );
 });
@@ -59,33 +62,7 @@ function Skills() {
   return (
     <section className={styles.skills}>
       <h2>Skills</h2>
-      <div className={styles.skills__grid}>
-        {skillItem}
-        {/* <div className={styles.skills__grid__item}>
-          <SiReact />
-          <p>React</p>
-        </div>
-        <div className={styles.skills__grid__item}>
-          <SiReact />
-          <p>React</p>
-        </div>
-        <div className={styles.skills__grid__item}>
-          <SiReact />
-          <p>React</p>
-        </div>
-        <div className={styles.skills__grid__item}>
-          <SiReact />
-          <p>React</p>
-        </div>
-        <div className={styles.skills__grid__item}>
-          <SiReact />
-          <p>React</p>
-        </div>
-        <div className={styles.skills__grid__item}>
-          <SiReact />
-          <p>React</p>
-        </div> */}
-      </div>
+      <div className={styles.skills__grid}>{skillItem}</div>
     </section>
   );
 }
